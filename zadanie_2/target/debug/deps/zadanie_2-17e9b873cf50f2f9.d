b23e27a114c1ea21e419f3b26cbc7c1f288b0128
Fusekla <miroslavgabik@gmail.com>
2020-12-10 19:11:49 +0100

    semtam

---
+++ zadanie_2/target/debug/deps/zadanie_2-17e9b873cf50f2f9.d
@@ -0,0 +1,5 @@
+d:\Rust\Advent_2020\zadanie_2\target\debug\deps\zadanie_2-17e9b873cf50f2f9.rmeta: src\main.rs
+
+d:\Rust\Advent_2020\zadanie_2\target\debug\deps\zadanie_2-17e9b873cf50f2f9.d: src\main.rs
+
+src\main.rs:
---
+++ zadanie_2/target/debug/deps/zadanie_2-b8c67cdf8712975a.d
@@ -0,0 +1,5 @@
+d:\Rust\Advent_2020\zadanie_2\target\debug\deps\zadanie_2-b8c67cdf8712975a.rmeta: src\main.rs
+
+d:\Rust\Advent_2020\zadanie_2\target\debug\deps\zadanie_2-b8c67cdf8712975a.d: src\main.rs
+
+src\main.rs:
