79e0bdd5cf79ad52b35b1b69906ac24eab14a67b
Fusekla <miroslavgabik@gmail.com>
2020-12-22 17:55:22 +0100

    6b done

--- zadanie_6/B/src/main.rs
+++ zadanie_6/B/src/main.rs
@@ -1,15 +1,25 @@
 fn main() {
-    let vstup_txt: Vec<_> = include_str!("sample.txt").split("\r\n\r\n").collect();
+    let vstup_txt: Vec<_> = include_str!("input.txt").split("\r\n\r").collect();
 
-    println!("{:?}", vstup_txt[0]);
+    let mut vsetci_suhlasia = 0;
 
-    let gejza:String = 8;
+    for skupina in vstup_txt {
+        let individualne:Vec<_> = skupina.split("\r").collect();
+        let initial_pismenka:Vec<_> = individualne[0].trim().chars().collect();
 
-    let jozko:String = 8; 
-
-    
-
-
-    
-
+            for kazde in initial_pismenka {
+                let mut i = 0;
+                let mut ciastkove = 0;
+                while i < individualne.len() {
+                    if individualne[i].contains(kazde) {
+                        ciastkove += 1;
+                    }
+                    i += 1;
+                }
+                if ciastkove == individualne.len() {
+                    vsetci_suhlasia +=1;
+            }
+        }
+    }
+    println!("Vsetci suhlasia : {}", vsetci_suhlasia);
 }
---
+++ zadanie_6/B/target/rls/debug/deps/B-cbaeddab1ee902d4.d
@@ -0,0 +1,6 @@
+c:\Users\MiroslavGabik\Documents\Rust\Advent_2020\zadanie_6\B\target\rls\debug\deps\B-cbaeddab1ee902d4.rmeta: src\main.rs src\input.txt
+
+c:\Users\MiroslavGabik\Documents\Rust\Advent_2020\zadanie_6\B\target\rls\debug\deps\B-cbaeddab1ee902d4.d: src\main.rs src\input.txt
+
+src\main.rs:
+src\input.txt:
