9bd2a606cbceeaf6665d6fc4908ab372ef896e79
Fusekla <miroslavgabik@gmail.com>
2020-12-24 23:10:06 +0100

    aids

--- zadanie_7/b/src/main.rs
+++ zadanie_7/b/src/main.rs
@@ -1,5 +1,4 @@
 fn main() {
-    let vstup_txt: Vec<_> = include_str!("input.txt").lines().collect();
     let gazda = spocitaj("shinygold", 1);
     println!("Finalny pocet vrecoch : {}", gazda);
 }
@@ -11,14 +10,15 @@ fn spocitaj(vrece: &str, pocet_vrecoch: u64) -> u64 {
         let members: Vec<_> = i.split(',').collect();
         if members[0] == vrece && members.len() != 1 {
             let mut j = 0;
-            if j < members.len()-1 {
+            while j < members.len() -3 {
                 println!("prva cast : {}, druha cast : {}", members[j+1], members[j+3]);
                 pocet_vrecoch = pocet_vrecoch * (members[j+1].parse::<u64>().unwrap() + members[j+3].parse::<u64>().unwrap());
                 println!("Pocet vrecoch : {}", pocet_vrecoch);
                 pocet_vrecoch = pocet_vrecoch + spocitaj(members[j+2], members[j+1].parse::<u64>().unwrap());
+                // pocet_vrecoch = pocet_vrecoch + spocitaj(members[j+4], members[j+3].parse::<u64>().unwrap());
                 j += 2;
             }
         }
     }
-    return pocet_vrecoch
+    return pocet_vrecoch;
 }
